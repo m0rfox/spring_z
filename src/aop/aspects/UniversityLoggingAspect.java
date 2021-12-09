f0c0d96393e954b36b5782abe864218a324cfa91
Valerii Glavatskih <glavatskihvalerii@yandex.ru>
2021-12-09 22:46:46 +0400

    Lesson 33

---
+++ src/aop/aspects/UniversityLoggingAspect.java
@@ -0,0 +1,42 @@
+package aop.aspects;
+
+import aop.Student;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@Aspect
+public class UniversityLoggingAspect {
+//
+//    @Before("execution(* getStudents())")
+//    public void beforeGetStudentsLoggingAdvice(){
+//        System.out.println("beforeGetStudentsLoggingAdvice: логируем получение " +
+//                "списка студентов методом getStudents");
+//    }
+//
+//    @AfterReturning(pointcut = "execution(* getStudents())"
+//            , returning = "students")
+//    public void afterGetStudentsLoggingAdvice(List<Student> students){
+//        Student firstStudent = students.get(0);
+//        String nameSurname = firstStudent.getNameSurname();
+//        nameSurname = "Mr. " + nameSurname;
+//        firstStudent.setNameSurname(nameSurname);
+//        double avgGrade = firstStudent.getAvgGrade();
+//        avgGrade =avgGrade + 1;
+//        firstStudent.setAvgGrade(avgGrade);
+//
+//        System.out.println("afterGetStudentsLoggingAdvice: логируем получение " +
+//                "списка студентов после работы методом getStudents");
+//    }
+
+    @AfterThrowing(pointcut = "execution(* getStudents())", throwing = "exception")
+    public void afterThrowingStudentsLoggingAdvice(Throwable exception){
+        System.out.println("afterThrowingStudentsLoggingAdvice: логируем выброс " +
+                "исключения " + exception);
+    }
+}
