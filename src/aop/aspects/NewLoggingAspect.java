e79b63560403fefb2ee0c968a587340e2703bac5
Valerii Glavatskih <glavatskihvalerii@yandex.ru>
2021-12-09 23:46:50 +0400

    Lesson 35

---
+++ src/aop/aspects/NewLoggingAspect.java
@@ -0,0 +1,26 @@
+package aop.aspects;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class NewLoggingAspect {
+
+    @Around("execution(public String returnBook())")
+    public Object aroundReturnBookLoggingAdvice(ProceedingJoinPoint proceedingJoinPoint)
+            throws Throwable {
+        System.out.println("aroundReturnBookLoggingAdvice: " +
+                "в библиотеку пытаются вернуть книгу");
+        long begin = System.currentTimeMillis();
+        Object targetMethodResult = proceedingJoinPoint.proceed();
+        long end = System.currentTimeMillis();
+        System.out.println("aroundReturnBookLoggingAdvice: " +
+                "в библиотеку успешно вернули книгу");
+        System.out.println("aroundReturnBookLoggingAdvice: выполнил" +
+                "работу за " + (end - begin) + " милисекунд");
+        return targetMethodResult;
+    }
+}
--- src/aop/aspects/UniversityLoggingAspect.java
+++ src/aop/aspects/UniversityLoggingAspect.java
@@ -1,10 +1,7 @@
 package aop.aspects;
 
 import aop.Student;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -34,9 +31,16 @@ public class UniversityLoggingAspect {
 //                "списка студентов после работы методом getStudents");
 //    }
 
-    @AfterThrowing(pointcut = "execution(* getStudents())", throwing = "exception")
-    public void afterThrowingStudentsLoggingAdvice(Throwable exception){
-        System.out.println("afterThrowingStudentsLoggingAdvice: логируем выброс " +
-                "исключения " + exception);
+//    @AfterThrowing(pointcut = "execution(* getStudents())", throwing = "exception")
+//    public void afterThrowingStudentsLoggingAdvice(Throwable exception){
+//        System.out.println("afterThrowingStudentsLoggingAdvice: логируем выброс " +
+//                "исключения " + exception);
+//    }
+
+    @After("execution(* getStudents())")
+    public void afterGetStudentLoggingAdvice(){
+        System.out.println("afterGetStudentLoggingAdvice: логируем нормальное " +
+                "окончание работы метода или выброс исключения");
     }
+
 }
